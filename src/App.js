a0b9eebc92895f5346f17bc1c29fafc27301dea7
Fauz Ahmed <fauz.ahmed369@gmail.com>
2019-11-13 22:56:16 -0600

    created signup route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
-import MainRouter from './MainRouter'
+import { BrowserRouter } from 'react-router-dom';
+import MainRouter from './MainRouter';
 
 const App = () => (
   <BrowserRouter>
